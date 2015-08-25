7b727a4caa0fecd4002cee9e9a63d4357a52770a
Chris Hare <chris.hare@arrisi.com>
2015-08-24 22:40:46 -0400

    Added basic Reviews and Actor Details Activities

---
+++ app/src/main/java/com/android/fourthwardcoder/popularmovies/Actor.java
@@ -0,0 +1,80 @@
+package com.android.fourthwardcoder.popularmovies;
+
+/**
+ * Created by chare on 8/24/2015.
+ */
+public class Actor {
+
+    String name;
+    String biography;
+    String birthday;
+    String deathday;
+    String homepage;
+    int id;
+    String birthPlace;
+    String profileImagePath;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getDeathday() {
+        return deathday;
+    }
+
+    public void setDeathday(String deathday) {
+        this.deathday = deathday;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public void setHomepage(String homepage) {
+        this.homepage = homepage;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBirthPlace() {
+        return birthPlace;
+    }
+
+    public void setBirthPlace(String birthPlace) {
+        this.birthPlace = birthPlace;
+    }
+
+    public String getProfileImagePath() {
+        return profileImagePath;
+    }
+
+    public void setProfileImagePath(String profileImagePath) {
+        this.profileImagePath = profileImagePath;
+    }
+}
